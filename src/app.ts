f203da379f0a93fce0ccdb2967fc1f20bc77afac
Muhammad hashim <mh4165209@gmail.com>
2026-07-19 01:16:47 +0500

    push code 02

--- src/app.ts
+++ src/app.ts
@@ -21,9 +21,22 @@ console.log(save7);
 
 function add(){
     let save8=+save1.value;
+    console.log(save8);
     let save9=+save2.value;
+    console.log(save9);
     let save10=save8 + save9;
-    let save11=save7.innerHTML=save10.toString();
-    return save11;
+    console.log(save10);
+    let save11=save7.innerHTML='Result ' +save10;
+    localStorage.setItem('saveInput1',save8.toString());
+    localStorage.setItem('saveInput2',save9.toString());
+    localStorage.setItem('saveInput3',save10.toString());
+    localStorage.setItem('saveInput4',save11)
+   let save12=  localStorage.getItem('saveInput1');
+    let save13= localStorage.getItem('saveInput2');
+    let save14= localStorage.getItem('saveInput3');
+    let save15= localStorage.getItem('saveInput4');
+
+    
+    // return save11;
 }
 save7.addEventListener('click',add)
